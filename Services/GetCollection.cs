50d6545d64184a9eead822d0ec42ab5c22fb9b82
Karina Hrama <hrama.karina@chnu.edu.ua>
2023-01-25 18:26:09 +0200

    first commit

---
+++ Services/GetCollection.cs
@@ -0,0 +1,22 @@
+﻿using MongoDB.Driver;
+
+namespace lab3.Services
+{
+    public class GetCollection<T>
+    {
+        private readonly ServiceConnection _serviceConnection;
+        private readonly IMongoCollection<T> _collection;
+        
+        public GetCollection(ServiceConnection serviceConnection)
+        {
+            _serviceConnection = serviceConnection;
+            _collection = _serviceConnection.MongoDatabase.GetCollection<T>(typeof(T).Name+"s");
+        }
+
+        public async Task<IEnumerable<T>> GetAll()
+        {
+            
+            return await _collection.Find(a => true).ToListAsync();
+        }
+    }
+}
---
+++ Services/ServiceConnection.cs
@@ -0,0 +1,17 @@
+﻿using lab3.Database;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+
+namespace lab3.Services
+{
+    public class ServiceConnection
+    {
+        public IMongoDatabase MongoDatabase { get; }
+
+        public ServiceConnection(IOptions<DbConnection> db)
+        {
+            var mongoClient = new MongoClient(db.Value.ConnectionString);
+            MongoDatabase = mongoClient.GetDatabase(db.Value.Database);
+        }
+    }
+}
